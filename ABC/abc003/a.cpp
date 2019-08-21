ac6479161b4c13d77b737e647e7a693c189f8a96
potyama <kamikaze117117@gmail.com>
2019-08-22 03:50:27 +0900

    A問題を埋めてく

--- ABC/abc003/a.cpp
+++ ABC/abc003/a.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-      int i=0,N;
-      
-      std::cin >> N;
-      i = (1 + N) * 10000 / 2;
-      std::cout << i << std::endl;
-}  
-      
+      int N,ans = 0;
+
+      cin >> N;
+
+      for(int i = 1; i <= N ; i++){
+            ans += (10000 * i);
+      }
+      ans /= N;
+      cout << ans << endl;
+}
