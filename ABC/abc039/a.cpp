0e7bb8eb9c21bfc36e4749d8540ef073f7bbf583
potyama <kamikaze117117@gmail.com>
2019-10-02 21:47:18 +0900

    now

---
+++ ABC/abc039/a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int A,B,C;
+      cin >> A >> B >> C;
+
+      cout << 2*(A*B+B*C+A*C) <<endl;
+}
---
+++ ABC/abc040/a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+      int n,x;
+
+      cin >> n >> x;
+
+      if(n-x > x-1)cout << x-1 << endl;
+      else cout << n-x << endl;
+}
