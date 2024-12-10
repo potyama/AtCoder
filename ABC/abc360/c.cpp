cba26c4832ade47e0a256521abfea945c61ddec4
pppp <kamikaze117117@gmail.com>
2024-12-11 01:25:54 +0900

    360まで解いた

--- ABC/abc360/c.cpp
+++ ABC/abc360/c.cpp
@@ -1,43 +1,27 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
+#define rep(i, a, b) for(int i=a; i<(b); i++)
+#define rrep(i, a, b) for(int i=a; i<=(b); i++)
+#define ll long long
+const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+
 int main(){
-    int N; cin >> N;
-    vector<int> A(N);
-    for(int i=0; i<N; i++){
-        cin >> A[i];
+    int N;cin >> N;
+    vector<int> A(N), W(N);
+    rep(i, 0, N)cin >> A[i];
+    rep(i, 0, N)cin >> W[i];
+    vector<int> tmp(N);
+    rep(i, 0, N){
+        A[i]--;
+        tmp[A[i]] = max(tmp[A[i]], W[i]);
     }
-    vector<int> W(N);
-    for(int i=0; i<N; i++){
-        cin >> W[i];
-    }
-    
-    sort(A.begin(), A.end());
-    sort(W.begin(), W.end());
-    int cnt=0, ans=0;
-    for(int i=0; i<=N-1; i++){
-        cout << "Check:" << A[i]<<"-"<<W[i] << "-" << cnt << endl;;
-        if(A[i] != A[i+1]){
-            if(cnt != 0){
-                vector<int> W_tmp(cnt);
-                for(int j=i-cnt; j<=i; j++){
-                    W_tmp[j] = W[j];
-                    cout << cnt << " " << W_tmp[j] << j << endl;
-                }
-                sort(W_tmp.begin(), W_tmp.end());
-                cout << W_tmp[0] << endl;
-                for(int k=0; k<cnt; k++){
-                    cout << ans + W_tmp[k] << endl;
-                    ans += W_tmp[k];
-                }
-                cnt = 0;
-                cout << "nowans=" << ans << endl;
-            }
-        }else{
-            cnt++;
-        }
-    }
-    cout << ans << endl;  
+    int sum_max = accumulate(tmp.begin(), tmp.end(), 0);
+    int sum = accumulate(W.begin(), W.end(), 0);
+    cout << sum - sum_max << endl;
+    return 0;
 }
